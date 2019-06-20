52f6873bec2dac02f5885a988ae298c8b7e635fe
Narrakas <netkas@users.noreply.github.com>
2019-06-19 23:08:56 -0400

    Inital Commit

---
+++ tests/example.php
@@ -0,0 +1,19 @@
+<?php
+
+    use acm\acm;
+    use acm\Objects\Schema;
+
+    $SourceDirectory = __DIR__ . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . 'src' . DIRECTORY_SEPARATOR;
+    include_once ($SourceDirectory . 'acm' . DIRECTORY_SEPARATOR . 'acm.php');
+
+    $acm = new acm(__DIR__, 'ACM Example');
+
+    $DatabaseConfigSchema = new Schema();
+    $DatabaseConfigSchema->setDefinition('host', 'localhost');
+    $DatabaseConfigSchema->setDefinition('port', '3306');
+    $DatabaseConfigSchema->setDefinition('username', 'root');
+    $DatabaseConfigSchema->setDefinition('password', '');
+
+    $acm->defineSchema('Database', $DatabaseConfigSchema);
+
+    var_dump($acm->getConfiguration('Database'));
